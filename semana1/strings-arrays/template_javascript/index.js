1d7b324fffa48074897e6b9f552bab0671a1061b
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-08-11 16:11:42 -0300

    Exercício aula5

---
+++ semana1/strings-arrays/template_javascript/index.js
@@ -0,0 +1,36 @@
+// EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+/*1.
+console.log('a. ', array) será: undefined
+console.log('b. ', array) será: null
+console.log('c. ', array.length) será: 11
+console.log('d. ', array[i]) será: 3
+console.log('e. ', array) substituirá o elemento número 1 da array por 19
+console.log('f. ', valor) mostrará o elemento número 6 da array*/
+
+/*2.
+SUBI NUM ONIBUS EM MIRROCOS 27*/
+
+//EXERCÍCIOS DE ESCRITA DE CÓDIGO
+/*1.
+let mail = prompt('Digite seu e-mail')
+let nome = prompt('Digite seu nome')
+console.log('O e-mail ' + mail + ' foi cadastrado com sucesso. Seja bem vindo ' + nome + '!')*/
+
+/*2.
+const comidas = ['sushi', 'shawarma', 'churrasco', 'lasanha', 'carne picada']
+console.log(comidas)
+console.log('Essas são minahs comidas favoritas:', comidas)
+let novaComida = prompt('Digite sua comida preferida')
+comidas[1] = novaComida
+console.log(comidas)*/
+
+/*3.
+let listaDeTarefas = []
+let tarefa1 = prompt('Cite uma tarefa')
+let tarefa2 = prompt('Cite uma segunda tarefa')
+let tarefa3 = prompt('Cite uma terceira tarefa')
+listaDeTarefas.push(tarefa1, tarefa2, tarefa3)
+console.log(listaDeTarefas)
+let check = prompt('Numerando as tarefas de 0 a 2, digite o número de uma que já realizou')
+listaDeTarefas.splice(check, 1)
+console.log(listaDeTarefas)*/
