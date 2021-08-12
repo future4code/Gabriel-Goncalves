ff132177375d51dbf9696b22557f5ccb95940182
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-08-12 16:13:10 -0300

    Exercícios aula 6

---
+++ semana1/funcoes/template_javascript/index.js
@@ -0,0 +1,90 @@
+//EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+/*1.
+a) Será impresso no console o número 10 e o número 50 em linhas separadas.
+b) A função será executada mas não exibirá resultado na tela nem no console.
+Não apareceria nada no console.*/
+
+/*2.
+a) Esta função pega um texto e, colocando todos os caracteres em minúsculos,
+procura a palavra cenoura.
+
+b) i. true
+  ii. true
+ iii. true*/
+
+//EXERCÍDIOS DE ESCRITA DE CÓDIGO
+
+/*1.
+a) function minhaFuncao() {
+    console.log('Sou Gabriel, 26 anos, aluno da Labenu.')
+}
+minhaFuncao()*/
+
+/*b)function tetraParametrada(nome, idade, cidade, profissão) {
+    console.log('Eu sou ' + nome + ' tenho ' + idade + ' anos, moro em ' + cidade + ' e sou ' + profissão + '.')
+}
+
+nome = 'Atanásio'
+idade = 77
+cidade = 'Alexandria'
+profissão = 'Santo'
+
+tetraParametrada(nome, idade, cidade, profissão)*/
+
+/*2.
+a) function somaDois(num1, num2) {
+    return num1 + num2
+}
+
+console.log(somaDois(2, 3))*/
+
+/*b) function booleana(num1, num2) {
+    console.log(num1 > num2)
+    return num1 > num2
+}
+
+booleana(30, 5)*/
+
+/*c) function truePar(num1) {
+    return (num1 % 2) === 0
+}
+
+console.log(truePar(7))*/
+
+
+/*-----------------------NÃO CONSEGUI ----------------
+Não consegui imprimir no console o texto em UpperCase e com o número de caracteres
+consegui fazer com UpperCase ou length mas não com ambos.
+d)function imprimeEmCaps(texto) {
+    return texto.length.toUpperCase()
+}
+
+console.log(imprimeEmCaps('laia laia laie laia'))
+
+/*3.
+function soma(num1, num2) {
+    return num1 + num2
+}
+
+function subtração(num1, num2) {
+    return num1 - num2
+}
+
+function multiplicação(num1, num2) {
+    return num1 * num2
+}
+
+function divisão(num1, num2) {
+    return num1 / num2
+}
+
+num1 = Number(prompt('Digite um número'))
+num2 = Number(prompt('Digite outro número'))
+
+console.log(
+    soma(num1, num2),
+    subtração(num1, num2),
+    multiplicação(num1, num2),
+    divisão(num1, num2)
+    )*/
