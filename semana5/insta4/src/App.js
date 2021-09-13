d03e5bd1347c3d0990c6546eed4251840b97c641
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-09-12 22:42:46 -0300

    Tentativa de resolução dos exercícios

--- semana5/insta4/src/App.js
+++ semana5/insta4/src/App.js
@@ -10,26 +10,38 @@ const MainContainer = styled.div`
 `
 
 class App extends React.Component {
+  state = {
+    postsRecolocados: [
+      {
+        nomeUsuario: 'paulinha',
+        fotoUsuario: 'https://picsum.photos/50/50',
+        fotoPost: 'https://picsum.photos/200/150'
+      },
+      {
+        nomeUsuario: 'lorival',
+        fotoUsuario: 'https://i.picsum.photos/id/351/50/50.jpg?hmac=5-oFGrlcRCd0RTV7EOyGeXFU0x8SjfyWvWHatGeQeBA',
+        fotoPost: 'https://i.picsum.photos/id/400/200/150.jpg?hmac=wKjdRjyAZu6tPoEhl8AtH4gUDQHfRfwp5cHTjWPyRFQ'
+      },
+      {
+        nomeUsuario: 'donizete',
+        fotoUsuario: 'https://i.picsum.photos/id/724/50/50.jpg?hmac=VMK53Oyno-1J5fS9K4l4LB-M4eMEJsublUDq5801BNA',
+        fotoPost: 'https://i.picsum.photos/id/84/200/150.jpg?hmac=gcABdgufdz9Kc-4ncDzg0snrTuDYIXZrVfeJbrY2DQU'
+      }
+    ]
+  }
   render() {
+    const listaDePosts = this.state.postsRecolocados.map((objeto) => {
+      this.setState(
+        <Post>
+          {objeto.nomeUsuario}
+          {objeto.fotoUsuario}
+          {objeto.fotoPost}
+        </Post>
+      )
+    })
     return (
         <MainContainer>
-          <Post
-            nomeUsuario={'paulinha'}
-            fotoUsuario={'https://picsum.photos/50/50'}
-            fotoPost={'https://picsum.photos/200/150'}
-          />
-        
-          <Post
-            nomeUsuario={'lorival'}
-            fotoUsuario={'https://i.picsum.photos/id/351/50/50.jpg?hmac=5-oFGrlcRCd0RTV7EOyGeXFU0x8SjfyWvWHatGeQeBA'}
-            fotoPost={'https://i.picsum.photos/id/400/200/150.jpg?hmac=wKjdRjyAZu6tPoEhl8AtH4gUDQHfRfwp5cHTjWPyRFQ'}
-          />
-        
-          <Post
-            nomeUsuario={'donizete'}
-            fotoUsuario={'https://i.picsum.photos/id/724/50/50.jpg?hmac=VMK53Oyno-1J5fS9K4l4LB-M4eMEJsublUDq5801BNA'}
-            fotoPost={'https://i.picsum.photos/id/84/200/150.jpg?hmac=gcABdgufdz9Kc-4ncDzg0snrTuDYIXZrVfeJbrY2DQU'}
-          />
+            <div>{listaDePosts}</div>
         </MainContainer>
     );
   }
