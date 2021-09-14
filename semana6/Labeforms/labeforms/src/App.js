c2bd14aab02deb7599113b29972f41b02842034c
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-09-13 23:26:21 -0300

    Exercício aula 21

---
+++ semana6/Labeforms/labeforms/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import DadosGerais from './Components/DadosGerais';
+import Educacionais from './Components/Educacionais';
+import Gerais from './Components/Gerais'
+import Fim from './Components/Fim'
+import React from 'react';
+
+class App extends React.Component {
+  state = {
+    etapaUmConcluida: false,
+    etapaDoisConcluida: false,
+    etapaTresConcluida: false
+  }
+  render (){
+    return (
+      <div className="App">
+        <Fim/>
+      </div>
+    )
+  }
+}
+
+export default App;
