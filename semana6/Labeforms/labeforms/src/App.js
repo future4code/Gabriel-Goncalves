b26fe7ee54f5b0e999d49de4c1cb57ecbbd02008
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-09-13 23:32:18 -0300

    correção exercícios aula 21

--- semana6/Labeforms/labeforms/src/App.js
+++ semana6/Labeforms/labeforms/src/App.js
@@ -14,6 +14,9 @@ class App extends React.Component {
   render (){
     return (
       <div className="App">
+        <DadosGerais/>
+        <Educacionais/>
+        <Gerais/>
         <Fim/>
       </div>
     )
