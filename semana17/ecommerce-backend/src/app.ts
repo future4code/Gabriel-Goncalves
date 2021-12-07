22d3fe5df7dc44b98a00cb78bdd59b295657d355
Gabriel Luiz Gonçalves <blgoncalves@hotmail.com>
2021-12-06 23:03:08 -0300

    Continuando labcommerce

--- semana17/ecommerce-backend/src/app.ts
+++ semana17/ecommerce-backend/src/app.ts
@@ -1,20 +1,10 @@
 import express, { Express } from "express"
 import cors from "cors"
-import createUsers from "./endpoints/createUsers"
-import getUsers from "./endpoints/getUsers"
-import createProduct from "./endpoints/createProduct"
-import getProducts from "./endpoints/getProducts"
 
 const app: Express = express()
 
 app.use(express.json())
 app.use(cors())
-
-app.post("/users", createUsers)
-app.get("/users", getUsers)
-
-app.post("/products", createProduct)
-app.get("/products", getProducts)
 
 app.listen(3003, () => {
     console.log("Servidor pronto!")
--- semana17/ecommerce-backend/src/index.ts
+++ semana17/ecommerce-backend/src/index.ts
@@ -1,3 +1,14 @@
 import app from "./app";
+import createProduct from "./endpoints/createProduct";
+import createPurchases from "./endpoints/createPurchases";
+import createUsers from "./endpoints/createUsers";
+import getProducts from "./endpoints/getProducts";
+import getUsers from "./endpoints/getUsers";
 
-app.post("/users")
+app.post("/users", createUsers)
+app.get("/users", getUsers)
+
+app.post("/products", createProduct)
+app.get("/products", getProducts)
+
+app.post("/purchases", createPurchases)
